4d410aa97889f8ccd43ab67c09260e4c710ce598
Luca Tenuta <luca.tenuta@gmail.com>
2021-10-22 18:37:26 -0400

    Removed print
    Removed print statements (debug purpose)

--- extensions/default.py
+++ extensions/default.py
@@ -4,9 +4,7 @@ import pathlib
 def config():
     """Fetchs config file"""
     path = pathlib.Path.cwd()
-    print(path)
     for file in path.glob(f"*.json"):
-        print(file)
         try:
             with open(file, encoding='utf8') as data:
                 return json.load(data)
