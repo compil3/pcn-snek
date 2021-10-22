a57f678b6047e5423397d09ca5beb202a313c430
Luca Tenuta <luca.tenuta@gmail.com>
2021-10-22 18:54:49 -0400

    Cleaned up and added Select
    Added a Select that displays all current active Scales.

--- scales/admin.py
+++ scales/admin.py
@@ -22,48 +22,43 @@ from dis_snek.models.context import InteractionContext
 
 
 import logging
-import sys
 import pathlib
+from extensions import default
+
+config = default.config()
 
 guild_id = 689119429375819951
-admin_perm = [Permission(842505724458172467, 1, True)]
+staff_only = Permission(842505724458172467, 1, True),
 
 
 class Admin(Scale):
 
     scales_list = []
     path = pathlib.Path.cwd()
-    print(path)
     for file in path.glob("scales/*.py"):
         scales_list.append(file.stem)
 
-    # FIXME: fix reload command
-    @slash_command("reload", description="Reload all Scales", scope=guild_id)
-    @slash_permission(guild_id=guild_id, permissions=admin_perm)
+
+    @slash_command("reload", description="Reload all Scales", scope=guild_id, default_permission=False)
+    @slash_permission(guild_id=guild_id, permissions=staff_only)
     @slash_option("scale", "Scale to reload", 3, required=False)
     @slash_option("reload_all", "Reload all Scales", 5, required=False)
     async def reload(self, ctx, **kwargs):
         embed = Embed(title="Reloading all Scales!", color=0x808080)
-
+        
         print(kwargs)
         for filename in listdir("./scales"):
-
-            if kwargs is None:
+            
+            if kwargs is None:        
                 if filename.endswith(".py") and not filename.startswith("_"):
                     try:
                         self.bot.regrow_scale(f"scales.{filename[:-3]}")
                         print(f"Loaded scales.{filename[:-3]}")
                         logging.info(f"scales.{filename[:-3]} loaded.")
-                        embed.add_field(
-                            name=f"Reloaded: ``{filename}``",
-                            value="\uFEFF",
-                            inline=False,
-                        )
+                        embed.add_field(name=f"Reloaded: ``{filename}``", value="\uFEFF", inline=False)
                     except Exception as e:
                         print(f"Failed to load scale {filename[:-3]}: {e}")
-                        embed.add_field(
-                            name=f"Failed to load ``{filename}``", value=e, inline=False
-                        )
+                        embed.add_field(name=f"Failed to load ``{filename}``", value=e, inline=False)
             elif kwargs == filename:
                 if ".py" not in kwargs:
                     module = kwargs + ".py"
@@ -73,39 +68,17 @@ class Admin(Scale):
                     return
                 else:
                     pass
+
         await ctx.send(embeds=[embed], ephemeral=True)
 
     @reload.error
-    async def reload_error(self, e, *args, **kwargs):
-        logging.ERROR(f"{args=}\n\n{kwargs=}")
-
-    @slash_command("load", description="Reload a single Scale", scope=guild_id)
-    @slash_permission(permissions=admin_perm, guild_id=guild_id)
-    @slash_option("scale", "Scale to load/reload", 3, required=True, choices=None)
-    async def load(self, ctx, scale: str):
-        embed = Embed(title=f"Attempting to reload {scale}", color=0x808080)
-        try:
-            if ".py" not in scale:
-                module = scale + ".py"
-            else:
-                ...
-            for scale_file in listdir("./scales"):
-                if module == scale_file:
-                    self.bot.regrow_scale(f"scales.{scale_file[:-3]}")
-                    embed.add_field(name="Loaded {scale}", value="\uFEFF", inline=False)
-                    await ctx.send(embeds=[embed], ephemeral=True)
-                else:
-                    pass
-        except ExtensionNotFound as ef:
-            logging.error(f"Extension {module} has not been found: {ef}.")
-
-    @load.error
     async def load_error(self, e: ExtensionNotFound, *args, **kwargs):
         logging.error(f"load.error caught failure: {e}\n{args=}\n{kwargs=}")
 
     # TODO: add a reload single scale command
 
-    @slash_command("scales", description="List all Scales", scope=guild_id)
+    @slash_command("scales", description="Staff Only", scope=guild_id, default_permission=False)
+    @slash_permission(guild_id=guild_id, permissions=staff_only)
     async def scale_builder(self, ctx: InteractionContext, **kwargs):
         await ctx.defer(ephemeral=True)
         try:
@@ -123,28 +96,24 @@ class Admin(Scale):
         except Exception as e:
             print(e)
 
-    @component_callback(custom_id="ScalesList")
+    @component_callback("ScalesList")
     async def scalelist_callback(self, ctx: InteractionContext, **kwargs):
         await ctx.defer(ephemeral=True)
         selected_scale = ctx.data["data"]["values"][0]
         embed = Embed(title=f"Attempting to reload {selected_scale}", color=0x808080)
         if selected_scale in Admin.scales_list:
-            self.bot.regrow_scale(f"scales.{ctx.data['data']['values'][0]}")
-            embed.add_field(name=f"Reloaded Scale", value=selected_scale, inline=False)
-            await ctx.edit_origin(embeds=[embed])
+            try:
+                self.bot.regrow_scale(f"scales.{ctx.data['data']['values'][0]}")
+                embed.add_field(name=f"Reloaded Scale", value=selected_scale, inline=False)
+                await ctx.edit_origin(embeds=[embed])
+            except ScaleLoadException:
+                self.bot.grow_scale(f"scales.{ctx.data['data']['values'][0]}")
+                embed.add_field(name=f"Reloaded Scale", value=selected_scale, inline=False)
+                await ctx.edit_origin(embeds=[embed])
+
+
         else:
-            print("Nothing Worked.")
-
-
-# TODO: Pass relative path in list
-# def BuildScales():
-#     scales_list = []
-#     path = pathlib.Path.cwd()
-#     print(path)
-#     for file in path.glob('scales/*.py'):
-#         print(file.__str__())
-#         scales_list.append(file.__str__())
-#     return scales_list
+                print("Nothing Worked.")
 
 
 def setup(bot):
