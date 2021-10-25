20640294e12c6750b144341a82bf8990f4ee3bc5
Luca Tenuta <luca.tenuta@gmail.com>
2021-10-25 01:25:08 -0400

    Updated events for membership screening

--- scales/events.py
+++ scales/events.py
@@ -1,22 +1,23 @@
 import logging
 
-from dis_snek.models import listen
-from dis_snek.models.scale import Scale
-from dis_snek.models.listener import listen
-from dis_snek.models.discord_objects.embed import Embed
-
 import motor.motor_asyncio as motor
-
+from dis_snek.models import listen
+from dis_snek.models.discord_objects.embed import Embed
+from dis_snek.models.discord_objects.user import Member
+from dis_snek.models.listener import listen
+from dis_snek.models.scale import Scale
 from extensions import default
+from extensions import auto_verify
 
 config = default.config()
 
-cluster = motor.AsyncIOMotorClient(config['mongo_connect'])
+cluster = motor.AsyncIOMotorClient(config["mongo_connect"])
 db = cluster["Nation"]
 collection = db["registered"]
+verify = auto_verify.verify
+
 
 class Events(Scale):
-
     @listen()
     async def on_message_create(self, event):
 
@@ -46,27 +47,40 @@ class Events(Scale):
                 logging.info(
                     f"\n{message.author.display_name} deleted a message.\nContent: {message.content}"
                 )
-    
 
     # TODO: Add message on member join
-    # @listen()
-    # async def on_raw_member_remove(self, event):
-    #     print(event)
-    #     # print(f"\n\n{event.guild.name}")
-    #     # print(f"\n\n{event.guild.id}")
-    
-    
-    # @listen()
-    # async def on_raw_member_add(self, event):
-    #     print(event)
-    #     print(f"\n\n{event.guild.name}")
-    #     print(f"\n\n{event.guild.id}")
-    # # when a member joins send them an embed
+    @listen()
+    async def on_member_remove(self, event):
+        print(event)
+        pass
+        # print(f"\n\n{event.guild.name}")
+        # print(f"\n\n{event.guild.id}")
 
-    # # @listen()
-    # # async def raw_guild_member_add(self, event: GuildEvent):
-    # #         print(event)
-    # #         print(event.member)
+    @listen()
+    async def on_member_update(self, event):
+        role_id = 843896103686766632
+
+        if event.before.pending and event.after.pending is True:
+            pass
+        elif event.after.pending is False and event.before.pending is True:
+            if verify(event.after.display_name) is False:
+                embed = Embed(
+                    title="Welcome to Pro Clubs Nation Discord",
+                    description="Unfortunately your Discord username does not match any records.  Therefore you will have to add yourself to our verification queue by typing **/add [GamerTag]** inside of **#new-member-verification** and waiting until you are verified.  Thank you for your patience.",
+                )
+            else:
+                embed = Embed(
+                    title="Welcome to Pro Clubs Nation Discord",
+                    description="You have been automatically verified as your Discord name matches our records.  If this is incorrect please let a Moderator know.  Failure to do so will result in termination of access to our Discord.  Thank you and play nice.",
+                )              
+                await event.after.add_role(role_id)
+            await event.after.send(embeds=[embed])
+        else:
+            pass
+
+    @listen()
+    async def on_member_add(self, event):
+        ...
 
 
 def setup(bot):
