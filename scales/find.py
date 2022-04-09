6c95ba6c69fb8461932755ad8909bac11037cf8c
Luca Tenuta <luca.tenuta@gmail.com>
2022-04-09 00:56:36 -0400

    Added reg data

--- scales/find.py
+++ scales/find.py
@@ -17,6 +17,7 @@ from dis_snek.models import (
 )
 from dis_snek.models.snek.context import InteractionContext
 from dis_snek.models.discord.enums import CommandTypes
+from beanie import Document
 from dotenv import load_dotenv
 from extensions import default
 from config import load_settings
@@ -33,7 +34,24 @@ admin_perm = [Permission(842505724458172467, 1, True)]
 # TODO: Make the data retrieval async in all user facing commands
 
 
+class Registered(Document):
+    user_id: int
+    discord_full_name: str
+    registered_gamer_tag: str
+    pcn_url: str
+    registered_date: str
+
+    class Collection:
+        name = "discord_registered"
+
 class PlayerFinder(Scale):
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.config = load_settings()
+        # config = self.config
+
+    
     def D_Embed(self, title: str) -> Embed:
         e = Embed(
             f"PCN Player Lookup: {title}",
@@ -61,31 +79,27 @@ class PlayerFinder(Scale):
     @slash_option("gamertag", "Enter Gamertag to check", 3, required=True)
     async def find(self, ctx: InteractionContext, gamertag: str):
         """Finds a player using /find [gamertag]"""
-        await ctx.send(f"Searching for {gamertag}...", ephemeral=True)
+        # await ctx.send(f"Searching for {gamertag}...", ephemeral=True)
+        await ctx.defer(ephemeral=True)
         # try:
-        url = default_player_url.format(gamertag)
+        url = self.config.urls.find_player.format(f"{gamertag}&_fields=title,link,date,modified,slug")
         try:
             r = requests.get(url)
             if r.status_code == 200:
                 if r.json()[0] is None:
                     raise IndexError
                 else:
-                    player_found = requests.get(
-                        config["urls"]["find_player"].format(
-                            f"{gamertag}&_fields=title,link,date,modified,slug"
-                        )
-                    )
                     e = self.D_Embed(
-                        f"**{player_found.json()[0]['title']['rendered']}**"
+                        f"**{r.json()[0]['title']['rendered']}**"
                     )
                     e.add_field(
-                        "Registered", player_found.json()[0]["date"], inline=True
+                        "Registered", r.json()[0]["date"], inline=True
                     )
                     e.add_field(
-                        "Last Updated", player_found.json()[0]["modified"], inline=True
+                        "Last Updated", r.json()[0]["modified"], inline=True
                     )
-                    e.add_field("Slug", player_found.json()[0]["slug"])
-                    e.add_field("PCN Profile", player_found.json()[0]["link"])
+                    e.add_field("Slug", r.json()[0]["slug"])
+                    e.add_field("PCN Profile", r.json()[0]["link"])
                     await ctx.send(embeds=[e])
             else:
                 e = self.D_Embed(f"Connection Error")
@@ -98,13 +112,7 @@ class PlayerFinder(Scale):
 
 
     #TODO Use the register command to add users to database.  Then pull the gamer tag from the database and use it to find the player.
-    @context_menu(
-        "Search",
-        CommandTypes.USER,
-        scopes=[
-            689119429375819951,442081251441115136
-        ],        
-    )
+    @context_menu("Search",CommandTypes.USER,scopes=[689119429375819951,442081251441115136])
     @slash_permission(
         # Permission(910676210172977213, 174342051318464512, PermissionTypes.ROLE, True),  # Player, TheNine
         # Permission(442081251441115136, 449043802829750272,  PermissionTypes.ROLE, True), # Player, PCN Discord
@@ -115,32 +123,32 @@ class PlayerFinder(Scale):
         Finds selected player when right clicking>Apps>Lookup
         """
         await ctx.defer(ephemeral=True)
+
         member = self.bot.get_member(ctx.target_id, ctx.guild_id)
-        print(str(member.display_name))
-        url = default_player_url.format(member.display_name)
+        find_user = await Registered.find_one(Registered.user_id == member.id)
+        
         try:
-            r = requests.get(url)
+            r = requests.get(self.config.urls.find_player.format(f"{member.display_name}&_fields=title,link,date,modified,slug"))
             if r.status_code == 200:
                 if r.json()[0] is None:
                     raise IndexError
                 else:
-                    player_found = requests.get(
-                        config["urls"]["find_player"].format(
-                            f"{member.display_name}&_fields=title,link,date,modified,slug"
-                        )
-                    )
                     e = self.D_Embed(
-                        f"**{player_found.json()[0]['title']['rendered']}**"
+                        f"**{r.json()[0]['title']['rendered']}**"
                     )
+                    if find_user is None:
+                        e.description= f"{member.display_name} has not registered with the PCN Discord Bot."
+                    else:
+                        e.add_field("Registered GT", find_user.registered_gamer_tag, inline=False)
                     e.add_field("Discord ID", str(member))
                     e.add_field(
-                        "Registered", player_found.json()[0]["date"], inline=True
+                        "Registation Date", r.json()[0]["date"], inline=False
                     )
                     e.add_field(
-                        "Last Updated", player_found.json()[0]["modified"], inline=True
+                        "Last Updated", r.json()[0]["modified"], inline=False
                     )
-                    e.add_field("Slug", player_found.json()[0]["slug"])
-                    e.add_field("PCN Profile", player_found.json()[0]["link"])
+                    e.add_field("API Slug", r.json()[0]["slug"])
+                    e.add_field("PCN Profile", r.json()[0]["link"])
                     await ctx.send(embeds=[e])
             else:
                 e = self.D_Embed(f"Connection Error")
@@ -149,8 +157,10 @@ class PlayerFinder(Scale):
         except IndexError:
             e = self.D_Embed(f"Results")
             e.description = f"**{member}** not found"
+            e.add_field("Registered GT", "Not Registered with Bot", inline=False)
             await ctx.send(embeds=[e])
 
 
 def setup(bot):
     PlayerFinder(bot)
+    bot.add_model(Registered)
